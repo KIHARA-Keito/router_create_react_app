b30b101a5d541976259a9b2585693d50adf73db9
KIHARA Keito <kiharakeito@gmail.com>
2020-01-16 22:44:59 +0900

    add router sample

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Redirect } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>Home</div>
+  );
+}
+/*
+    <Redirect to="/about/" />
+*/
+
+export default Home;
